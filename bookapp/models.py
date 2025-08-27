844cf8dc7aaa83f83bedb275c077f13df2448a6b
Suiseich <aarontayag89@gmail.com>
2025-08-27 12:23:31 +0800

    Update code

--- bookapp/models.py
+++ bookapp/models.py
@@ -1,1 +1,14 @@
+from django.db import models
 
+# Create your models here.
+
+class bookapp(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    isbn = models.IntegerField(max_length=100)
+    published_date = models.DateTimeField(auto_now_add=True)
+    genre = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}{self.author}"
